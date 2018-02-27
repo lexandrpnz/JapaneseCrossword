b7340b7cacfb9378f2f72b4932ecb92996986fc3
Lexandr <leshiy_al@mail.ru>
2018-02-27 21:38:11 +0300

    Исправлен алгоритм решения с использованием конечного автомата

--- JapaneseCrossword/JCClasses/StateMachine/SolverWithStateMachine.cs
+++ JapaneseCrossword/JCClasses/StateMachine/SolverWithStateMachine.cs
@@ -6,16 +6,13 @@ namespace JCClasses
     public class SolverWithStateMachine: SolverBase
     {
         private IMath Math = new CachedMath();
-        private Int64 max;
-        private Int64 current;
 
         protected override bool Solvered(Byte[] row, Byte[] data)
         {
             Byte FreeCellSize = CalcFreeCellSize((Byte)row.Length, data);
-            max = Math.GetVar(data.Length, FreeCellSize);
-            current = 0;
-            return Attempt(0, row, data);
+            return Attempt(row, data);
         }
+
         private Byte CalcFreeCellSize(Byte RowCount, Byte[] Data)
         {
             Byte FreeCellSize = (Byte)RowCount;
@@ -31,14 +28,14 @@ namespace JCClasses
             StateMachine<Int32, byte> ret = new StateMachine<Int32, byte>();
             int currentState = 0;
             ret.Add(currentState, 100, currentState);
-            ret.Add(currentState, 1, currentState+1);
+            ret.Add(currentState, 1, currentState + 1);
             currentState++;
 
             foreach (var current in data)
             {
                 for (byte cell = 1; cell < current; cell++)
                 {
-                    ret.Add(currentState, cell == current ? (byte)100 : (byte)1, currentState+1);
+                    ret.Add(currentState, cell == current ? (byte)100 : (byte)1, currentState + 1);
                     currentState++;
                 }
                 ret.Add(currentState, 100, currentState);
@@ -46,69 +43,78 @@ namespace JCClasses
                 currentState++;
             }
             ret.CurrentState = 0;
-            ret.EndState = currentState-1;
+            ret.EndState = currentState - 1;
             return ret;
         }
 
-        private bool Attempt(int index, Byte[] row, Byte[] data)
+        private bool Attempt(Byte[] row, Byte[] data)
         {
-            if(index == row.Length)
-            {
-                current++;
-                Debug.WriteLine("Attempt: {0}/{1}", current, max);
-                StateMachine<Int32, byte> res = CreateStateMachine(data);
-                try
-                {
-                    foreach (var current in row)
-                    {
-                        res.Next(current);
-                    }
-                }
-                catch (System.Exception)
-                {
-                    return false;
-                }
-                return res.EndState == res.CurrentState;
-            }
-
-            for (int i = index; i < row.Length; i++)
+            bool ret = false;
+            for (int i = 0; i < row.Length; i++)
             {
                 if (0 != row[i])
                 {
                     continue;
                 }
+
                 row[i] = 1;
-                bool isBlock = Attempt(i + 1, row, data);
+                bool isBlock = IsFill(row, data);
                 row[i] = 100;
-                bool isBan = Attempt(i + 1, row, data);
+                bool isBan = IsFill( row, data);
                 row[i] = 0;
 
-                if (isBan && isBlock)
-                {
-                    continue;
-                }
-
-                if (!isBan && !isBlock)
-                {
-                    return false;
-                }
-
-                if(0 != index)
+                if (!(isBan ^ isBlock))
                 {
                     continue;
                 }
 
                 if (isBlock)
                 {
+                    ret = true;
                     row[i] = 1;
                 }
                 if (isBan)
                 {
+                    ret = true;
                     row[i] = 100;
                 }
             }
-            return true;
+            return ret;
         }
 
+        private bool IsFill(Byte[] row, Byte[] data)
+        {
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (0 != row[i])
+                {
+                    continue;
+                }
+
+                row[i] = 1;
+                bool isBlock = IsFill(row, data);
+                row[i] = 100;
+                bool isBan = IsFill(row, data);
+                row[i] = 0;
+                return isBan || isBlock;
+            }
+            return Check(data, row);
+        }
+        private bool Check(Byte[] data, Byte[] row)
+        {
+            StateMachine<Int32, byte> res = CreateStateMachine(data);
+            try
+            {
+                foreach (var current in row)
+                {
+                    res.Next(current);
+                }
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+            return res.EndState == res.CurrentState;
+        }
     }
 }
